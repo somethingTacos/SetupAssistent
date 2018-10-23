d58e37053b38572d78dae8fd1f6cf82d311c3c00
John Omelko <somethingTacos@hotmail.com>
2018-10-23 15:42:18 -0400

    Some ModuleControl changes.

--- SetupAssistent/CustomControls/ModuleControl.cs
+++ SetupAssistent/CustomControls/ModuleControl.cs
@@ -4,21 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SetupAssistent.CustomControls
 {
-    /// <summary>
-    /// Interaction logic for ModuleButton.xaml
-    /// </summary>
-    public partial class ModuleButton : Button
+    public class ModuleButton : System.Windows.Controls.Button
     {
         public static readonly DependencyProperty IsExpandedProperty =
             DependencyProperty.Register("IsExpanded", typeof(bool), typeof(ModuleButton), new PropertyMetadata(false));
@@ -27,11 +16,6 @@ namespace SetupAssistent.CustomControls
         {
             get { return (bool)GetValue(IsExpandedProperty); }
             set { SetValue(IsExpandedProperty, value); }
-        }
-
-        public ModuleButton()
-        {
-            InitializeComponent();
         }
     }
 }
