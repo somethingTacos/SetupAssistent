9c77d21e626f985e3cd6b71de2c69ad783ea4f22
John Omelko <somethingTacos@hotmail.com>
2018-10-28 21:04:29 -0400

    Some Settings updates.

--- SetupAssistent/ViewModel/SettingsViewModel.cs
+++ SetupAssistent/ViewModel/SettingsViewModel.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using SetupAssistent.Model;
 
 namespace SetupAssistent.ViewModel
 {
     class SettingsViewModel
     {
+        public Settings settings { get; set; }
+        public MyICommand SaveCommand { get; set; }
 
         #region Default Constructor
         public SettingsViewModel()
         {
+            SaveCommand = new MyICommand(onSaveCommand, canSaveCommand);
+            settings = AllSettings.settings[0];
+        }
+        #endregion
 
+        #region Commands Code
+        public void onSaveCommand(object parameter)
+        {
+            MessageBox.Show("Testing Save Command");
+        }
+        public bool canSaveCommand()
+        {
+            return true;
         }
         #endregion
     }
